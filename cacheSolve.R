2f069583eaa3ecfbf8afa4ebd564e55fb4bfb15c
varamd <varamd@gmail.com>
2014-05-25 11:55:02 -0600

    Create cacheSolve.R
    This function computes the inverse of the special "matrix" returned by makeCacheMatrix

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s  ## Returns 
+}
